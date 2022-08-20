2c2bc22dfb3c3a5b81f6abe82c0fdaa3fa275564
magnae2016 <magnae2016@gmail.com>
2022-08-20 12:31:16 +0900

    Add repository interface

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
         System.out.println("HELLO TODO");
